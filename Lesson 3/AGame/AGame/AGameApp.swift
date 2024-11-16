ebe5b6654dcb44516fd504e6fe5a8c188d737792
Cesar Pasillas <42359065+CesarPasillas@users.noreply.github.com>
2024-11-16 16:51:00 -0600

    Lesson 3 - A game

---
+++ Lesson 3/AGame/AGame/AGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGameApp.swift
+//  AGame
+//
+//  Created by Cesar Pasillas on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
